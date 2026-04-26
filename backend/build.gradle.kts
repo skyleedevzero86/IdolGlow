6e6810b86ec87e92fad3b7a83019a03b7f48cef5
이경용 <sleekydz86@naver.com>
2026-04-26 22:47:38 +0900

    chore: spring-boot-jackson2 추가 및 JSON 매퍼를 Jackson2로 지정
    - spring-boot-jackson2 의존성 추가로 Jackson 2 ObjectMapper 자동 구성 활성화
    - MVC/WebFlux가 Jackson 2를 쓰도록 preferred-json-mapper를 jackson2로 설정

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-jackson2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-security")
